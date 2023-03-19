f16ecba0ff67fcc3f30d3c0a0a3c1ce5822671cd
grkb <chazpelo2@gmail.com>
2023-03-19 16:15:05 -0400

    early progress

---
+++ src/components/footer.tsx
@@ -0,0 +1,18 @@
+// @ts-ignore
+export default function Footer() {
+	return (
+		<>
+			<footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+				<p className="col-md-4 mb-0 text-muted">© 2023 Qobo Contributors</p>
+
+				<ul className="nav col-md-4 justify-content-end">
+					<li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Home</a></li>
+					<li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Features</a></li>
+					<li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Pricing</a></li>
+					<li className="nav-item"><a href="#" className="nav-link px-2 text-muted">FAQs</a></li>
+					<li className="nav-item"><a href="#" className="nav-link px-2 text-muted">About</a></li>
+				</ul>
+			</footer>
+		</>
+	)
+}
